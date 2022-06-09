2570470fb004c5aab0d0f4c06dbbb8f1b4aef3e9
aie06 <76236769+aie06@users.noreply.github.com>
2022-06-10 06:03:09 +0800

    Update

---
+++ CodingExam1/CodingExam1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CodingExam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input a string: ");
+                string word = Console.ReadLine().ToLower();
+
+                Console.Write("OUTPUT: " + toWeirdCase(word));
+            }
+           
+        }
+
+        public static string toWeirdCase(string word)
+        {
+            string final_word = "";
+            string initial_word = "";
+            string word_temp = "";
+            string[] words_container = word.Split(" ");
+
+            for (int i = 0; i< words_container.Length; i++)
+            {
+                word_temp = words_container[i];
+                initial_word = "";
+                for (int y=0; y< words_container[i].Length; y++)
+                {
+                    if (y % 2 == 0)
+                    {
+                       initial_word += char.ToUpper(word_temp[y]);
+                    }
+                    else
+                    {
+                        initial_word += word_temp[y];
+                    }
+                }
+                final_word += initial_word +" ";              
+            }
+            return final_word;
+        }
+    }
+}
---
+++ DuplicateNumbers/DuplicateNumbers/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuplicateNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input letters: ");
+                string word = Console.ReadLine();
+                Console.WriteLine(DuplicateNumbers(word));
+            }
+           
+        }
+        public static string DuplicateNumbers(string letters)
+        {
+          
+            var lets = letters.ToLower().ToCharArray();
+          
+            Dictionary<char, int> charMap = new Dictionary<char, int>();
+
+            foreach (var ch in lets)
+            {
+                if (charMap.ContainsKey(ch))
+                {
+                    charMap[ch] = charMap[ch] + 1;
+                }
+                else
+                {
+                    charMap.Add(ch, 1);
+                }
+            }
+
+            var keys = new HashSet<char>(charMap.Keys);
+
+            string fword = "";
+            string temp = "";
+            int counter = 0;
+            foreach (var ch in keys)
+            {
+                if (charMap[ch] > 1)
+                {
+                    counter++;
+                    fword = counter + " characters repeat more than once";
+                    temp += "'"+ch+"' " + "occurs " + charMap[ch] + " times.\n" ;               
+                }
+            }
+            if (counter == 0)
+            {
+                fword = "0 # no characters repeat more than once";
+            }
+
+            return fword + "\n" +temp;
+            
+        }
+
+    }
+}
